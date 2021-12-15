e4440e7f6200ec3f97c3615424a17f1fff72e74f
Linco <Linco@LAPTOP-UB8TUREC>
2021-12-15 02:07:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+            int outronum = 5;
+
+
+            Console.WriteLine(" Quanto é " + numero + "+" + outronum + "?");
+            
+            int resposta = Convert.ToInt32(Console.ReadLine());
+
+            if(resposta == 15)
+            {
+                Console.WriteLine("Parabens Você Acertou!!");
+            }
+            else
+            {
+                Console.WriteLine(" Tente Novamente !!");
+            }
+
+        }
+    }
+}
